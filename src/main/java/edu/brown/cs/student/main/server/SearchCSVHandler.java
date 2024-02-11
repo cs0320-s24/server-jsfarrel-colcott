6fc671903764229b95f899f6f14d3e455cfca04a
charlieolcott <charles_olcott@brown.edu>
2024-02-10 19:04:51 -0500

    feat: viewCSVHandler implementation

--- src/main/java/edu/brown/cs/student/main/server/SearchCSVHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/SearchCSVHandler.java
@@ -7,7 +7,9 @@ import edu.brown.cs.student.main.csv.CreatorFromRow;
 import edu.brown.cs.student.main.csv.ParserState;
 import edu.brown.cs.student.main.exception.FactoryFailureException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -25,7 +27,6 @@ public class SearchCSVHandler implements Route {
       String toSearch = request.queryParams("toSearch");
       String columnSpecifierString = request.queryParams("columnSpecifier");
       ColumnSpecified columnSpecifier;
-      //TODO: should index be capitalized?
       if(columnSpecifierString.equals("index")){
         columnSpecifier = ColumnSpecified.INDEX;
       }else if (columnSpecifierString.equals("name")){
@@ -37,9 +38,11 @@ public class SearchCSVHandler implements Route {
       String headerParam = request.queryParams("hasHeaders");
       boolean hasHeaders = headerParam.equals("true");
       CSVSearcher searcher = new CSVSearcher(this.parserState.getParser(), hasHeaders);
-      return searcher.search(toSearch, columnIdentifier, columnSpecifier);
+      Map<String, Object> responseMap = new HashMap<>();
+      responseMap.put("code", 200);
+      responseMap.put("search results", searcher.search(toSearch, columnIdentifier, columnSpecifier));
+      return ResponseBuilder.mapToJson(responseMap);
     }catch (FactoryFailureException e){
-      //TODO: put a useful message
       return ResponseBuilder.buildException(404, "File has malformed csv data.");
     }
   }
--- src/main/java/edu/brown/cs/student/main/server/ViewCSVHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/ViewCSVHandler.java
@@ -1,6 +1,12 @@
 package edu.brown.cs.student.main.server;
 
+import edu.brown.cs.student.main.csv.CSVSearcher;
+import edu.brown.cs.student.main.csv.CSVSearcher.ColumnSpecified;
 import edu.brown.cs.student.main.csv.ParserState;
+import edu.brown.cs.student.main.exception.FactoryFailureException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -14,6 +20,17 @@ public class ViewCSVHandler implements Route {
 
   @Override
   public Object handle(Request request, Response response) {
-    return "unimplemented";
+    try {
+      // create response
+      Map<String, Object> responseMap = new HashMap<>();
+      responseMap.put("code", 200);
+      responseMap.put("CSV data", this.parserState.getParser().parse());
+      return ResponseBuilder.mapToJson(responseMap);
+    }catch (FactoryFailureException e){
+      return ResponseBuilder.buildException(404, "File has malformed csv data.");
+    } catch (IOException e) {
+      //TODO: put a useful message
+      return ResponseBuilder.buildException(404, "Unable to read from file.");
+    }
   }
 }

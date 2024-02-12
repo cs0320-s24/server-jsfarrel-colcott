b35dd6665e6604d21e5dbc033db45608bbc54d97
John Farrell <jsfarrell12@gmail.com>
2024-02-12 09:20:00 -0500

    feat: cache proxy

--- src/main/java/edu/brown/cs/student/main/server/Server.java
+++ src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -2,12 +2,16 @@ package edu.brown.cs.student.main.server;
 
 import static spark.Spark.after;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.LoadingCache;
 import edu.brown.cs.student.main.csv.ParserState;
 import edu.brown.cs.student.main.server.broadband.ACSBroadbandSource;
 import edu.brown.cs.student.main.server.broadband.BroadbandHandler;
 import edu.brown.cs.student.main.server.csv.LoadCSVHandler;
 import edu.brown.cs.student.main.server.csv.SearchCSVHandler;
 import edu.brown.cs.student.main.server.csv.ViewCSVHandler;
+import java.util.concurrent.TimeUnit;
+import spark.Request;
 import spark.Spark;
 
 public class Server {
@@ -23,8 +27,11 @@ public class Server {
 
     ParserState parser = new ParserState();
 
-    // Setting up the handler for the GET /loadcsv, /viewcsv, /searchcsv
-    Spark.get("broadband", new BroadbandHandler(new ACSBroadbandSource()));
+    CacheBuilder<Object, Object> cacheBuilder = CacheBuilder.newBuilder()
+        .expireAfterWrite(30, TimeUnit.SECONDS);
+
+    // Setting up the handler for the GET /loadcsv, /viewcsv, /searchcsv, /broadband
+    Spark.get("broadband", new APICache(new BroadbandHandler(new ACSBroadbandSource()), cacheBuilder));
     Spark.get("loadcsv", new LoadCSVHandler(parser));
     Spark.get("viewcsv", new ViewCSVHandler(parser));
     Spark.get("searchcsv", new SearchCSVHandler(parser));

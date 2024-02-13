b8a282dc363a37007e70005bf2e6b8c1918a7e88
John Farrell <jsfarrell12@gmail.com>
2024-02-13 14:08:08 -0500

    chore: remove println

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -9,7 +9,7 @@ public final class Main {
    * @param args An array of command line arguments
    */
   public static void main(String[] args) {
-    System.out.println("hi");
+
   }
 
   private Main(String[] args) {}

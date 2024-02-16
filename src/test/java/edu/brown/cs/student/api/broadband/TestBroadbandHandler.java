97d2fcf61311cd4e73d29290ac0d45d74ae75753
charlieolcott <charles_olcott@brown.edu>
2024-02-15 19:23:06 -0500

    feat: added cache testing involving checking the date/time is the same for two successive querires

--- src/test/java/edu/brown/cs/student/api/broadband/TestBroadbandHandler.java
+++ src/test/java/edu/brown/cs/student/api/broadband/TestBroadbandHandler.java
@@ -1,16 +1,20 @@
 package edu.brown.cs.student.api.broadband;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import com.google.common.cache.CacheBuilder;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Types;
 import edu.brown.cs.student.main.server.broadband.BroadbandHandler;
+import edu.brown.cs.student.main.server.cache.APICache;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import okio.Buffer;
@@ -35,8 +39,13 @@ public class TestBroadbandHandler {
 
   @BeforeEach
   public void setup() {
+    CacheBuilder<Object, Object> cacheBuilder =
+        CacheBuilder.newBuilder().expireAfterWrite(30, TimeUnit.SECONDS);
     // Re-initialize parser, state, etc. for every test method
-    Spark.get("/broadband", new BroadbandHandler(new MockBroadbandSource(50.0))); // no need to mock
+    Spark.get(
+        "/broadband",
+        new APICache(
+            new BroadbandHandler(new MockBroadbandSource(50.0)), cacheBuilder)); // no need to mock
     Spark.awaitInitialization(); // don't continue until the server is listening
 
     Moshi moshi = new Moshi.Builder().build();
@@ -104,7 +113,91 @@ public class TestBroadbandHandler {
   }
 
   @Test
-  public void testBroadbandExtraParam() throws IOException {
+  public void testBroadbandSuccessCaching() throws IOException, InterruptedException {
+    String params = "state=California&county=Kings";
+
+    HttpURLConnection broadbandConnection1 = this.tryRequest("broadband?" + params);
+    assertEquals(200, broadbandConnection1.getResponseCode()); // successful *connection*
+    try (Buffer b1 = new Buffer().readFrom(broadbandConnection1.getInputStream())) {
+      Map<String, Object> responseBody1 = this.adapter.fromJson(b1);
+      Assert.notNull(responseBody1);
+      this.showDetailsIfError(responseBody1);
+      assertEquals("success", responseBody1.get("result"));
+
+      Thread.sleep(5000);
+      HttpURLConnection broadbandConnection2 = this.tryRequest("broadband?" + params);
+      assertEquals(200, broadbandConnection2.getResponseCode()); // successful *connection*
+      Buffer b2 = new Buffer().readFrom(broadbandConnection2.getInputStream());
+      Map<String, Object> responseBody2 = this.adapter.fromJson(b2);
+      Assert.notNull(responseBody2);
+      this.showDetailsIfError(responseBody2);
+      assertEquals("success", responseBody2.get("result"));
+
+      assertEquals(responseBody1.get("date"), responseBody2.get("date")); // same date/time
+
+      Thread.sleep(31000);
+      HttpURLConnection broadbandConnection3 = this.tryRequest("broadband?" + params);
+      assertEquals(200, broadbandConnection3.getResponseCode()); // successful *connection*
+      Buffer b3 = new Buffer().readFrom(broadbandConnection3.getInputStream());
+      Map<String, Object> responseBody3 = this.adapter.fromJson(b3);
+      Assert.notNull(responseBody3);
+      this.showDetailsIfError(responseBody3);
+      assertEquals("success", responseBody2.get("result"));
+
+      assertNotEquals(responseBody1.get("date"), responseBody3.get("date")); // different date/time
+      assertNotEquals(responseBody2.get("date"), responseBody3.get("date")); // different date/time
+
+      broadbandConnection1.disconnect();
+      broadbandConnection2.disconnect();
+      broadbandConnection3.disconnect();
+    }
+  }
+
+  @Test
+  public void testBroadbandExtraParam() throws IOException, InterruptedException {
+    String state = "California";
+    String county = "Kings";
+    String params = "state=" + state + "&county=" + county + "&extra=param";
+
+    HttpURLConnection broadbandConnection1 = this.tryRequest("broadband?" + params);
+    assertEquals(200, broadbandConnection1.getResponseCode()); // successful *connection*
+    try (Buffer b = new Buffer().readFrom(broadbandConnection1.getInputStream())) {
+      Map<String, Object> responseBody1 = this.adapter.fromJson(b);
+      Assert.notNull(responseBody1);
+      this.showDetailsIfError(responseBody1);
+      assertEquals("success", responseBody1.get("result"));
+
+      Thread.sleep(5000);
+      HttpURLConnection broadbandConnection2 = this.tryRequest("broadband?" + params);
+      assertEquals(200, broadbandConnection2.getResponseCode()); // successful *connection*
+      Buffer b2 = new Buffer().readFrom(broadbandConnection2.getInputStream());
+      Map<String, Object> responseBody2 = this.adapter.fromJson(b2);
+      Assert.notNull(responseBody2);
+      this.showDetailsIfError(responseBody2);
+      assertEquals("success", responseBody2.get("result"));
+
+      assertEquals(responseBody1.get("date"), responseBody2.get("date")); // same date/time
+
+      Thread.sleep(31000);
+      HttpURLConnection broadbandConnection3 = this.tryRequest("broadband?" + params);
+      assertEquals(200, broadbandConnection3.getResponseCode()); // successful *connection*
+      Buffer b3 = new Buffer().readFrom(broadbandConnection3.getInputStream());
+      Map<String, Object> responseBody3 = this.adapter.fromJson(b3);
+      Assert.notNull(responseBody3);
+      this.showDetailsIfError(responseBody3);
+      assertEquals("success", responseBody2.get("result"));
+
+      assertNotEquals(responseBody1.get("date"), responseBody3.get("date")); // different date/time
+      assertNotEquals(responseBody2.get("date"), responseBody3.get("date")); // different date/time
+
+      broadbandConnection1.disconnect();
+      broadbandConnection2.disconnect();
+      broadbandConnection3.disconnect();
+    }
+  }
+
+  @Test
+  public void testBroadbandExtraParamCaching() throws IOException {
     String state = "California";
     String county = "Kings";
     String params = "state=" + state + "&county=" + county + "&extra=param";
